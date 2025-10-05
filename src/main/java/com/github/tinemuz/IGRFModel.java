ea0bc3ca1a2b7f597ca4c9a66ac71bbacbfee856
tine <99731290+tinemuz@users.noreply.github.com>
2025-10-05 09:49:56 +0100

    Update license to MIT in source files and add NOTICE for derivative works

--- src/main/java/com/github/tinemuz/IGRFModel.java
+++ src/main/java/com/github/tinemuz/IGRFModel.java
@@ -1,3 +1,30 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 tinemuz
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * This file contains derivative works. See the NOTICE file for attribution and
+ * original license information:
+ * https://github.com/tinemuz/igrf-model/blob/main/NOTICE
+ */
 package com.github.tinemuz;
 
 import java.io.BufferedReader;
@@ -11,7 +38,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Allocation-free IGRF (International Geomagnetic Reference Field) evaluator using spherical 
+ * IGRF (International Geomagnetic Reference Field) evaluator using spherical
  * harmonic expansion to degree 13. Computes Earth's magnetic field vector and derived quantities
  * (declination, inclination, intensity) at any geodetic location and time.
  * 
@@ -36,7 +63,7 @@ import org.slf4j.LoggerFactory;
  *
  * <p><b>Performance:</b>
  * <ul>
- *   <li>Zero allocation after first call per thread (~20KB workspace cached)</li>
+ *   <li>Zero allocation after first call per thread</li>
  *   <li>Thread-safe via ThreadLocal (no locks, no contention)</li>
  *   <li>Typical: 5-15 μs per evaluation</li>
  * </ul>
